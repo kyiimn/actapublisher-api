62e869646012e76098b351d41c308ad6d2d5b3b9
Kim Yin <kyiimn@gmail.com>
2021-01-20 16:07:14 +0900

    기본구성

---
+++ src/App.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+class App {
+    public application: express.Application;
+
+    constructor() {
+        this.application = express();
+        this.router();
+    }
+
+    private router(): void {
+        this.application.get('/', (req: express.Request, res: express.Response) => {
+            res.send('hello!');
+        });
+    }
+}
+export default App;
