3334a8a34d1bdd1561235971e23eaca288b3acbb
Kim Yin <kyiimn@gmail.com>
2021-02-23 16:12:15 +0900

    샘플코드 수정

--- schema/code.sql
+++ schema/code.sql
@@ -87,7 +87,10 @@ INSERT INTO t_config_storage_def (id, name, base_path, archive) VALUES (5, 'FONT
 
 INSERT INTO t_config_page_size_def
  (name, paper_type, paper_width, paper_height, paper_direction, linespacing_size, linespacing_unit, linespacing_ratio, col_margin_inside, col_margin_outside, col_count, col_size, col_spacing, col_other, col_total_size, line_margin_top, line_margin_bottom, line_height, line_count, line_spacing, line_other, line_total_size) 
- VALUES ('기본판형', 'CUSTOM', 400, 548, 'V', 9.5, 'POINT', 100, 20.01, 20.01, 6, 55.63, 5, 1.2, 400, 3.9, 3.9, 3.35, 106, 1.5, 27.6, 548);
+ VALUES ('기본판형', 'CUSTOM', 400, 548, 'V', 9.5, 'POINT', 100, 20.01, 20.01, 6, 55.63, 5, 1.2, 400, 18,915, 18.915, 3.35, 106, 1.5, 0, 548);
+INSERT INTO t_config_page_size_def
+ (name, paper_type, paper_width, paper_height, paper_direction, linespacing_size, linespacing_unit, linespacing_ratio, col_margin_inside, col_margin_outside, col_count, col_size, col_spacing, col_other, col_total_size, line_margin_top, line_margin_bottom, line_height, line_count, line_spacing, line_other, line_total_size) 
+ VALUES ('기본판형2', 'CUSTOM', 323, 470, 'V', 9.5, 'POINT', 100, 12, 12, 5, 56.6, 4, 0, 323, 13.224, 19.285, 3.81, 80, 1.7, 0, 470);
 
 INSERT INTO t_account_dept (id, media_id, name, sort, invalid_flag, "group") VALUES (1, 1, '개발부', 1, 0, 0);
 INSERT INTO t_account_user (media_id, dept_id, login_name, name, password, use, level, rule, fixed) VALUES (1, 1, 'ykim', '김김', '1234', 1, 1, 1, 1);
